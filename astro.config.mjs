2dc7f8b15ddf79d2c6bf02e7fed2b164c39eddf6
domrishe <71892996+domrishe@users.noreply.github.com>
2023-05-13 15:14:10 -0400

    it's ready

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import image from "@astrojs/image";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://astronaut.github.io',
-    base: '/dom',
+  site: 'https://astronaut.github.io',
+  base: '/dom',
+  integrations: [image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })],
 });
-    
