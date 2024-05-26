baa40f84b29b6c7e907cbbef2d8d5457b00eced8
domrishe <71892996+domrishe@users.noreply.github.com>
2024-05-26 13:07:55 -0400

    changed root again

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import image from "@astrojs/image";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dom.energy',
-  base: '/dom',
+  base: '/',
   integrations: [image({
     serviceEntryPoint: '@astrojs/image/sharp'
   })],
