7972d573c996793e0c1cb57acbe6a2b749513be1
domrishe <71892996+domrishe@users.noreply.github.com>
2024-08-08 12:29:09 -0400

    😂

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from 'astro/config';
 
-import image from "@astrojs/image";
+// import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://domrishe.xyz',
   base: '/',
-  integrations: [image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  })],
+  integrations: 
+  [
+    // vite, {
+    //   assetsInclude: ['**/*.glb', '**/*.stl'],
+    // }
+  ],
+
 });
